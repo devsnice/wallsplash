203f434f725286fbab6f0e540c0f30a5b3d15d46
Borisov Semen <devsnice@gmail.com>
2017-11-26 23:59:29 +0300

    [+] - image::added icons

--- src/components/containers/GalleryContainer/GalleryImage/GalleryImage.js
+++ src/components/containers/GalleryContainer/GalleryImage/GalleryImage.js
@@ -3,19 +3,35 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Box, Flex } from 'grid-styled';
 
+import downloadIcon from './resourses/download.svg';
+import heartIcon from './resourses/heart.svg';
+
 import imageService from '../../../../services/imageService';
 
-const ImageWrapper = styled(Box)`position: relative;`;
-const ImageActions = styled(Flex)`
-  bottom: 0;
-  position: absolute;
-  height: 32px;
-  width: 100%;
-  padding: 0 10px;
+const ImageWrapper = styled(Box)`
+  position: relative;
 `;
+
+const ImageDescription = styled(Flex)`
+  bottom: 15px;
+  height: 24px;
+  position: absolute;
+  padding: 0 15px;
+  width: 100%;
+`;
+
+const ImageTitle = styled(Box)`
+  color: #fff;
+`;
+
+const ImageActions = styled(Flex)`
+  align-items: baseline;
+`;
+
 const ImageAction = styled(Box)`
   color: #fff;
   cursor: pointer;
+  margin-left: 16px;
 `;
 
 class GalleryImage extends Component {
@@ -42,17 +58,30 @@ class GalleryImage extends Component {
     });
   };
 
+  handleLikeImage = () => {
+    alert('Like image');
+  };
+
   render() {
     const { image } = this.props;
 
     return (
       <ImageWrapper>
         <img width="100%" src={image.urls.small} />
-        <ImageActions justify="flex-end">
-          <ImageAction onClick={this.handleSetImageAsWallpaper}>
-            Install
-          </ImageAction>
-        </ImageActions>
+
+        <ImageDescription justify="space-between">
+          <ImageTitle>{image.description || 'Image title'}</ImageTitle>
+
+          <ImageActions>
+            <ImageAction onClick={this.handleSetImageAsWallpaper}>
+              <img src={downloadIcon} />
+            </ImageAction>
+
+            <ImageAction onClick={this.handleLikeImage}>
+              <img src={heartIcon} />
+            </ImageAction>
+          </ImageActions>
+        </ImageDescription>
       </ImageWrapper>
     );
   }
